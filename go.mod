ee204406c267342dc57d0255da63a45b5a56a585
egordigitax <fantomaka@gmail.com>
2025-07-21 12:02:19 +0500

    default middlewares for fiber and grpc

--- go.mod
+++ go.mod
@@ -1,21 +1,37 @@
 module github.com/egordigitax/logger
 
-go 1.20
+go 1.23.0
+
+toolchain go1.24.3
 
 require (
 	github.com/egordigitax/zerolog-sentry v0.0.3
+	github.com/gofiber/fiber/v2 v2.52.9
 	github.com/rs/zerolog v1.31.0
 	github.com/urfave/cli/v2 v2.25.7
+	gitlab.findmykids.org/fmk-pkg/crypto v0.0.2
+	google.golang.org/grpc v1.73.0
+	google.golang.org/protobuf v1.36.6
 )
 
 require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/getsentry/sentry-go v0.21.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
 )
---
+++ middleware.go
@@ -0,0 +1,148 @@
+package logger
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog"
+	"gitlab.findmykids.org/fmk-pkg/crypto"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"strconv"
+	"time"
+)
+
+type UserIDGetter func(token string) string
+type LogLevelSpecifier func(logger *zerolog.Logger, err error) (*zerolog.Event, error)
+
+func FiberMiddleware(
+	baseLogger *zerolog.Logger,
+	userIdGetter UserIDGetter,
+	logLevelSpecifier LogLevelSpecifier,
+) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+
+		enriched := baseLogger.With().
+			Str("http.method", c.Method()).
+			Str("http.path", c.Path()).
+			Str("http.query", string(c.Request().URI().QueryString())).
+			Str("http.ip", c.IP()).
+			Str("http.user_agent", c.Get("User-Agent"))
+
+		c.Context().QueryArgs().VisitAll(func(k, v []byte) {
+			if string(k) == "u" {
+				userId := userIdGetter(string(v))
+				enriched = enriched.Str("user_id", userId)
+			}
+			enriched = enriched.Str(string(k), string(v))
+		})
+
+		enrichedLogger := enriched.Logger()
+
+		c.Locals("logger", &enrichedLogger)
+
+		err := c.Next()
+
+		log, _ := logLevelSpecifier(&enrichedLogger, err)
+
+		log.
+			Str("body", string(c.Request().Body())).
+			Str("response", string(c.Response().Body())).
+			Int("status", c.Response().StatusCode()).
+			Dur("resp_time", time.Since(start)).
+			Msg(fmt.Sprintf("%s handle http request", c.Path()))
+
+		return err
+	}
+}
+func GRPCInterceptor(logger *zerolog.Logger) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		resp, err := handler(ctx, req)
+		if err != nil {
+
+			tryGetUserId, ok := extractUserIDViaJSON(req)
+			if !ok {
+				tryGetUserId = "unknown"
+			}
+
+			logger.Error().
+				Str("method", info.FullMethod).
+				Str("user_id", tryGetUserId).
+				Interface("request", req).
+				Err(err).
+				Msg(fmt.Sprintf("%s handle http request", info.FullMethod))
+		}
+		return resp, err
+	}
+}
+
+func extractUserIDViaJSON(req interface{}) (string, bool) {
+	var jsonBytes []byte
+	var err error
+
+	if msg, ok := req.(proto.Message); ok {
+		jsonStr := protojson.Format(msg)
+		jsonBytes = []byte(jsonStr)
+	} else {
+		jsonBytes, err = json.Marshal(req)
+		if err != nil {
+			return "", false
+		}
+	}
+
+	var data map[string]any
+	if err := json.Unmarshal(jsonBytes, &data); err != nil {
+		return "", false
+	}
+
+	for _, key := range []string{"user_id", "userId", "id"} {
+		if val, ok := data[key]; ok {
+			switch v := val.(type) {
+			case string:
+				return v, true
+			case float64:
+				return strconv.FormatInt(int64(v), 10), true
+			}
+		}
+	}
+	return "", false
+}
+
+func DefaultLogLevelSpecifier(
+	logger *zerolog.Logger,
+	apiErrors []error,
+	err error,
+) (*zerolog.Event, error) {
+	if err == nil {
+		return logger.Info(), nil
+	}
+
+	var fiberErr *fiber.Error
+	if errors.As(err, &fiberErr) && fiberErr.Code == fiber.StatusNotFound {
+		return logger.Info().Err(err), fiberErr
+	}
+
+	for _, apiErr := range apiErrors {
+		if errors.Is(err, apiErr) {
+			return logger.Info().Err(apiErr), err
+		}
+	}
+
+	return logger.Info().Err(err), errors.New("internal server error")
+}
+
+func DefaultUserIDGetter(conf *crypto.Config) UserIDGetter {
+	return func(token string) string {
+		userId, _ := crypto.GetIDByToken(token, conf)
+		return strconv.FormatUint(userId, 10)
+	}
+}
